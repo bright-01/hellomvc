985561a6e8283b0d32043ed20da420d4722fbffc
mwchoen <mwcheon@cudo.co.kr>
2022-04-23 23:34:14 +0900

    logging2

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,16 +1,17 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class LogTestController {
 
-    private final Logger log = LoggerFactory.getLogger(LogTestController.class);
-
+//    private final Logger log = LoggerFactory.getLogger(LogTestController.class);
+// Slf4j 어노테이션이 자동으로 잡아줌. 위에 따로 로그를 선언해줄 필요 없다.
 
 
     @RequestMapping("/log-test")
@@ -18,6 +19,9 @@ public class LogTestController {
         String name = "spring!";
 
         System.out.println("name = " + name);
+
+        // 이렇게 사용하면 로그 레벨과 상관없이 리소스를 이용한 연산이 이루어짐... 이렇게 + 연산자를 사용하면 안된다.
+        log.trace("trace log = {}"+ name);
 
         log.trace("trace log = {}", name);
         log.info("info log = {}", name);
